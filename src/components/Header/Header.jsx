eb609199f3060191ae44a9b6a8fdd86c2b1b4a69
KibeTheProggrammer <147256088+KibeTheProggrammer@users.noreply.github.com>
2024-06-19 01:00:50 +0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,10 +9,10 @@ const Header = () => {
 
         <div className='container flex'>
             <div className='header-content'>
-                <h2 className='text-uppercase text-white op-07 fw-6 ls-2'>web design, branding, graphic</h2>
+                <h2 className='text-uppercase text-white op-07 fw-6 ls-2'>web development, Design, graphic</h2>
                 <h1 className='text-white fw-6 header-title'>Hello, I'm <span className='text-brown'>Web Developer & Graphic Designer</span></h1>
                 <div className='btn-groups flex'>
-                  <button type = "button" className='btn-item bg-brown fw-4 ls-2'>See Works</button>
+                  <button type = "button" className='btn-item bg-brown fw-4 ls-2'>Resume</button>
                   <button type = "button" className='btn-item bg-dark fw-4 ls-2'>Contact Me</button>
                 </div>
             </div>
